0094eda1b54d72d4919346a8aec78dc46a81d968
AlexMJ666 <921711730@qq.com>
2019-07-08 16:51:50 +0800

    header demo

---
+++ Travel/src/pages/home/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="header-left">返回</div>
+      <div class="header-input">输入城市/景点/游玩主题</div>
+      <div class="header-right">城市</div>
+    </div>
+
+  </div>
+</template>
+<script>
+
+  export  default {
+    name: 'HomeHeader'
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display :flex
+    line-height: 2.86rem
+    background #25a4bb
+    color white
+    .header-left
+      width: 2.64rem
+      float: left
+    .header-input
+      flex: 1
+      background white
+      border-radius .2rem
+      margin-top .42rem
+      margin-left .8rem
+      margin-right .8rem
+      color #cacaca
+      height 2rem
+      line-height 2rem
+    .header-right
+      width: 2.24rem
+      float: right
+      text-align center
+</style>
